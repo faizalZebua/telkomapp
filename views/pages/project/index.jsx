5f7a806ec7af1b4937f21c3baf530a8dae0aa3fc
faizalZebua <faizal.ahmad27@icloud.com>
2018-09-14 20:49:42 +0700

    [ADD] Card style

--- views/pages/project/index.jsx
+++ views/pages/project/index.jsx
@@ -4,7 +4,30 @@ export default class Project extends Component {
     render () {
         return (
             <div>
-                <h3>Ini home</h3>
+                <div className="row mb-20">
+                    <div className="col-sm-12">
+                        <div className="card">
+                            <h4>Que All Project</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <h4>Best Product Performance</h4>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <h4>Best Squad Performance</h4>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <h4>Talent</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
